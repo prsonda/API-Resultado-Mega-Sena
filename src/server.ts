f25b01b2c61eff618bef0bc22cf3f519e506fffd
prsonda <prsonda@gmail.com>
2023-02-15 14:30:49 -0300

    Criado o server para iniciar a aplicação

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import { app } from "./app";
+require("dotenv").config();
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+	console.info(`Aplicação iniciada na porta ${port}`);
+});
+
+process.on("SIGINT", () => {
+	server.close();
+	console.info("Aplicação encerrada");
+});
