5f7042e2ec0b5910eecbdac98a51368750641c70
prsonda <prsonda@gmail.com>
2023-02-15 14:32:42 -0300

    controlador que busca resultado do último sorteio

---
+++ src/controllers/megasena-results.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import { lastResult } from "../services/last_result";
+
+export const megasenaResults = async (req: Request, res: Response) => {
+	try {
+		return lastResult(res);
+	} catch (error) {
+		const err = error as Error;
+		console.log(err.message);
+	}
+};
