f2abe6f87b791ee2ee2fa7a5f0215d35039a6e3f
prsonda <prsonda@gmail.com>
2023-02-16 04:36:52 -0300

    correção bug retornando números do sorteio atual

--- src/services/consult-id.ts
+++ src/services/consult-id.ts
@@ -11,32 +11,66 @@ export const fetchResultById = async (req: Request, res: Response) => {
 
 		await page.goto(urlSite);
 
+		const searchCurrentContest = await Promise.all([
+			page.$$eval(".ng-binding", (el) => el.map((tex) => tex.innerHTML)),
+		]);
+
+		if (searchCurrentContest === undefined) {
+			return res
+				.status(500)
+				.json({ message: "Erro de servidor, faça a busca novamente" });
+		}
+
+		const currentContest = Number(searchCurrentContest[0][18].split(" ")[1]);
+
+		if (Number(id) > currentContest || Number(id) <= 0 || !Number(id)) {
+			return res.status(404).json({
+				message: "Concurso não encontrado, digite um concurso válido!",
+			});
+		}
+
 		const selectFieldSearch = await page.waitForSelector("#buscaConcurso");
 
 		const searchById = await page.type("#buscaConcurso", id);
 
 		const searchEnter = await Promise.all([page.keyboard.press("Enter")]);
 
-		const waitingForAnswer = await Promise.all([
-			page.$$eval(".numbers > li", (el) => el.map((tex) => tex.innerHTML)),
+		const contestNumber = await Promise.all([
 			page.$$eval(".ng-binding", (el) => el.map((tex) => tex.innerHTML)),
 		]);
 
-		const contestNumber = Number(waitingForAnswer[1][18].split(" ")[1]) - 1;
+		const numberOfWinners = await Promise.all([
+			page.$$eval(".ng-binding", (el) => el.map((tex) => tex.innerHTML)),
+		]);
 
-		const drawnNumbers = [
-			Number(waitingForAnswer[0][0]),
-			Number(waitingForAnswer[0][1]),
-			Number(waitingForAnswer[0][2]),
-			Number(waitingForAnswer[0][3]),
-			Number(waitingForAnswer[0][4]),
-			Number(waitingForAnswer[0][5]),
-		];
+		const waitingForAnswer = await Promise.all([
+			page.$$eval(".ng-binding", (el) => el.map((tex) => tex.innerHTML)),
+		]);
 
-		const winners = Number(waitingForAnswer[1][35].split(" ")[0]);
+		if (
+			contestNumber === undefined ||
+			numberOfWinners === undefined ||
+			waitingForAnswer === undefined
+		) {
+			return res
+				.status(500)
+				.json({ message: "Erro de servidor, faça a busca novamente" });
+		}
+
+		const contestPrevious = Number(waitingForAnswer[0][18].split(" ")[1]) - 1;
+
+		const contest: number = Number(waitingForAnswer[0][18].split(" ")[1]);
+
+		if (contest !== Number(id)) {
+			return res.status(422).json({
+				message: "Solicitação não processada a tempo, faça a busca novamente",
+			});
+		}
+
+		const winners = Number(waitingForAnswer[0][35].split(" ")[0]);
 
 		let award = Number(
-			waitingForAnswer[1][28]
+			waitingForAnswer[0][28]
 				.split(" ")[1]
 				.replace(".", "")
 				.replace(".", "")
@@ -47,7 +81,7 @@ export const fetchResultById = async (req: Request, res: Response) => {
 			award =
 				winners *
 				Number(
-					waitingForAnswer[1][35]
+					waitingForAnswer[0][35]
 						.split(" ")[4]
 						.replace(".", "")
 						.replace(".", "")
@@ -55,19 +89,22 @@ export const fetchResultById = async (req: Request, res: Response) => {
 				);
 		}
 
-		const dateContest = waitingForAnswer[1][18].split(" ")[2].slice(1, 11);
+		const dateContest = waitingForAnswer[0][18].split(" ")[2].slice(1, 11);
 
-		const contest = Number(waitingForAnswer[1][18].split(" ")[1]);
-
-		if (Number(id) > contest || Number(id) <= 0 || !Number(id)) {
-			return res.status(404).json({ message: "Concurso não encontrado" });
-		}
+		const drawnNumbers = [
+			Number(waitingForAnswer[0][20]),
+			Number(waitingForAnswer[0][21]),
+			Number(waitingForAnswer[0][22]),
+			Number(waitingForAnswer[0][23]),
+			Number(waitingForAnswer[0][24]),
+			Number(waitingForAnswer[0][25]),
+		];
 
 		const resultPresentation = {
 			bolasSorteadas: drawnNumbers,
 			premio: award, // premio principal 6 acertos
 			acumulou: winners === 0 ? true : false,
-			concursoAnterior: contestNumber,
+			concursoAnterior: contestPrevious,
 			concurso: contest,
 			dataConcurso: dateContest,
 		};
@@ -76,6 +113,8 @@ export const fetchResultById = async (req: Request, res: Response) => {
 
 		await browser.close();
 	} catch (error) {
+		const err = error as Error;
+		console.log(err.message);
 		return res
 			.status(500)
 			.json({ message: "Erro de servidor, faça a busca novamente" });
