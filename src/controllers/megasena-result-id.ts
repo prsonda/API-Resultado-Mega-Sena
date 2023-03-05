4faa89faa9db25a5f86158318aba990d6651bd6e
Paulo <prsonda@gmail.com>
2023-03-04 22:59:26 -0300

    feito

--- src/controllers/megasena-result-id.ts
+++ src/controllers/megasena-result-id.ts
@@ -2,10 +2,10 @@ import { Request, Response } from "express";
 import { fetchResultById } from "../services/consult-id";
 
 export const megasenaResultsId = async (req: Request, res: Response) => {
-	try {
-		fetchResultById(req, res);
-	} catch (error) {
-		const err = error as Error;
-		console.log(err.message);
-	}
+  try {
+    fetchResultById(req, res);
+  } catch (error) {
+    const err = error as Error;
+    console.log(err.message);
+  }
 };
--- src/controllers/megasena-results.ts
+++ src/controllers/megasena-results.ts
@@ -2,10 +2,10 @@ import { Request, Response } from "express";
 import { lastResult } from "../services/last_result";
 
 export const megasenaResults = async (req: Request, res: Response) => {
-	try {
-		return lastResult(res);
-	} catch (error) {
-		const err = error as Error;
-		console.log(err.message);
-	}
+  try {
+    return lastResult(res);
+  } catch (error) {
+    const err = error as Error;
+    console.log(err.message);
+  }
 };
