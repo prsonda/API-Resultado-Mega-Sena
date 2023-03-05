ec70e349262cd1c5980d9811548b9c02df37cb3f
Paulo <prsonda@gmail.com>
2023-03-04 21:41:39 -0300

    feito

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import { app } from "./app";
+require("dotenv").config();
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.info(`Aplicação iniciada na porta ${port}`);
+});
+
+process.on("SIGINT", () => {
+  server.close();
+  console.info("Aplicação encerrada");
 });
